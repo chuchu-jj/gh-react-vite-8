bf04c0e0a3d802c5db442f547019749daf8ee88e
chuchu-jj <aguilerajosej@gmail.com>
2023-07-16 12:25:25 -0400

    8vo actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'ptpps://chuchu-jj.github.io/',
+  base: 'ptpps://chuchu-jj.github.io/gh-react-vite-8/',
   plugins: [react()],
 })
